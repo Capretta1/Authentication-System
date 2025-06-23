70786fc03af719af6db734180179457ead36412f
Capretta1 <blessnutsua3@gmail.com>
2025-06-23 10:13:26 +0000

    mongodb

--- src/dbconfig/dbconfig.ts
+++ src/dbconfig/dbconfig.ts
@@ -1,5 +1,25 @@
 import mongoose from "mongoose";
 
-async function connectDB() {}
+async function connectDB() {
+  try {
+    mongoose.connect(process.env.MONGODB_URI!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
+
+    connection.on("error", (err) => {
+      console.log(
+        err,
+        "MongoDB connection error, Please make sure mongodb is running",
+        +err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log(error, "Something goes wrong");
+  }
+}
 
 export default connectDB;
