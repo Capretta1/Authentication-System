c263b7f34a431f22e946ab9ee47dd0203c4cd8ac
Capretta1 <blessnutsua3@gmail.com>
2025-06-24 00:35:47 +0000

    would be back

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -4,9 +4,8 @@ import Jwt from "jsonwebtoken";
 export const getDataFromToken = (request: NextRequest) => {
   try {
     const token = request.cookies.get("token")?.value || "";
-    const decodedToken = Jwt.verify(token, process.env.TOKEN_SECRET!);
-
-    return decodedToken;
+    const decodedToken:any = Jwt.verify(token, process.env.TOKEN_SECRET!);
+    return decodedToken.id;
   } catch (error: unknown) {
     let message = "Something went wrong";
     if (error instanceof Error) {
